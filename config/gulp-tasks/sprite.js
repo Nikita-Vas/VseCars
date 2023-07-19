26b590826a6db2284e2d4f87c5cdf4bf15254c04
Nikita Vas <korjuna@yandex.ru>
2023-07-19 11:43:51 +0300

    header, home

---
+++ config/gulp-tasks/sprite.js
@@ -0,0 +1,95 @@
+import svgSprite from 'gulp-svg-sprite';
+export const sprite = () => {
+  return app.gulp
+    .src(`${app.path.src.svgicons}`, {})
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: 'SVG',
+          message: 'Error: <%= error.message %>',
+        })
+      )
+    )
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: '../img/icons/icons.svg',
+            //example: true
+          },
+        },
+        shape: {
+          id: {
+            separator: '',
+            generator: 'svg-',
+          },
+          transform: [
+            {
+              svgo: {
+                plugins: [
+                  {
+                    name: 'removeAttrs',
+                    params: {
+                      attrs: ['class', 'data-name', 'fill', 'stroke.*'],
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+        svg: {
+          rootAttributes: {
+            style: 'display: none;',
+            'aria-hidden': true,
+          },
+          xmlDeclaration: false,
+        },
+      })
+    )
+    .pipe(app.gulp.dest(`${app.path.srcFolder}`));
+};
+
+// import svgSprite from "gulp-svg-sprite";
+// export const sprite = () => {
+// 	return app.gulp.src(`${app.path.src.svgicons}`, {})
+// 		.pipe(app.plugins.plumber(
+// 			app.plugins.notify.onError({
+// 				title: "SVG",
+// 				message: "Error: <%= error.message %>"
+// 			}))
+// 		)
+// 		.pipe(svgSprite({
+// 			mode: {
+// 				symbol: {
+// 					sprite: '../img/icons/icons.svg',
+// 					//example: true
+// 				}
+// 			},
+// 			shape: {
+// 				id: {
+// 					separator: '',
+// 					generator: 'svg-'
+// 				},
+// 				transform: [
+// 					{
+// 						svgo: {
+// 							plugins: [
+// 								{ removeXMLNS: true },
+// 								{ convertPathData: false },
+// 								{ removeViewBox: false },
+// 							]
+// 						}
+// 					}
+// 				]
+// 			},
+// 			svg: {
+// 				rootAttributes: {
+// 					style: 'display: none;',
+// 					'aria-hidden': true
+// 				},
+// 				xmlDeclaration: false
+// 			}
+// 		}))
+// 		.pipe(app.gulp.dest(`${app.path.srcFolder}`));
+// }
